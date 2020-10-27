ec14ad0bfa4fd36324658839f9cdc8fed5944370
David Asabina <vid@bina.me>
2020-10-27 12:28:31 +0100

    refactor: Format with hlint

--- xmonad.hs
+++ xmonad.hs
@@ -32,7 +32,7 @@ myFocussedBorderColor = "#FF1493" -- "#e43a67" -- "#3abce4"
 myNormalBorderColor = "#1B1D1E"
 
 nextTag :: (String -> String)
-nextTag string = xmobarColor "red" "" (id string)
+nextTag string = xmobarColor "red" "" string
 
 myXmobarPP :: PP
 myXmobarPP =
