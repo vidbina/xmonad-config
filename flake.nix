dedede7dd96b9e0166ed99143c74f04a5a29c355
David Asabina <vid@bina.me>
2021-09-03 11:38:30 +0200

    fix: Nest configuration under config attr

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "XMonad Flake";
   outputs = { self }: {
     nixosModule = { config, ... }: username: {
-      home-manager.users."${username}".xsession.windowManager.xmonad = {
+      config.home-manager.users."${username}".xsession.windowManager.xmonad = {
         enable = true;
         enableContribAndExtras = true;
         config = ./xmonad.hs;
