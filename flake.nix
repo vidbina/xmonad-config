45ef32ed9d0c89d28c6b81c6619f1b7370a9063e
David Asabina <vid@bina.me>
2021-09-03 15:46:20 +0200

    refactor: Drop prefix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,11 @@
 {
   description = "XMonad Flake";
   outputs = { self }: {
-    nixosModule = { config, username, ... }: {
-      config.home-manager.users."${username}".xsession.windowManager.xmonad = {
-        enable = true;
-        enableContribAndExtras = true;
-        config = ./xmonad.hs;
-      };
+    # xsession.windowManager.xmonad
+    nixosModule = { config, ... }: {
+      enable = true;
+      enableContribAndExtras = true;
+      config = ./xmonad.hs;
     };
   };
 }
